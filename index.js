1e4459d60dda1a88674882c6d829c6464e9f1ec1
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-07-05 17:38:35 +0530

    adding loggers and error handler

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
-import express  from 'express'
+import express  from 'express';
+import morgan from 'morgan';
 
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT
+
+//logging middleware
+if(process.env.NODE_ENV ==="development"){
+    app.use(morgan('dev'))
+}
+
+
+//Body Parser Middleware
+app.use(express.json({limit: '10kb'}));
+app.use(express.urlencoded({extended:true,limit:"10kb"}));
+
+//Global Error Handler
+app.use((err,req,res,next) =>{
+    console.log(err.stack)
+    res.status(err.status || 500).json({
+        status:"error",
+        message:err.message || "Internal Server error",
+        ...err(process.env.NODE_ENV === "development" &&  { stack: err.stack}),
+    });
+});
+
+
+//Api Routes
+
+
+
+//404 handler
+app.use((req,res) =>{
+    res.status(404).json({
+        status:"error",
+        message: "Route not found!!",
+    });
+});
 
 app.listen(PORT, () =>{
     console.log(`server is running at ${PORT} in ${process.env.NODE_ENV} mode`)
