e067f23b7a4ab3bef079c762604b20694088be68
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-07-15 08:31:27 +0530

    healthcheck routes updated

--- routes/purchaseCourse.route.js
+++ routes/purchaseCourse.route.js
@@ -1,24 +1,24 @@
-import express from "express";
-import {
-  getCoursePurchaseStatus,
-  getPurchasedCourses,
-  handleStripeWebhook,
-  initiateStripeCheckout,
-} from "../controllers/coursePurchase.controller.js";
-import { isAuthenticated } from "../middleware/auth.middleware.js";
+// import express from "express";
+// import {
+//   getCoursePurchaseStatus,
+//   getPurchasedCourses,
+//   handleStripeWebhook,
+//   initiateStripeCheckout,
+// } from "../controllers/coursePurchase.controller.js";
+// import { isAuthenticated } from "../middleware/auth.middleware.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-  .route("/checkout/create-checkout-session")
-  .post(isAuthenticated, initiateStripeCheckout);
-router
-  .route("/webhook")
-  .post(express.raw({ type: "application/json" }), handleStripeWebhook);
-router
-  .route("/course/:courseId/detail-with-status")
-  .get(isAuthenticated, getCoursePurchaseStatus);
+// router
+//   .route("/checkout/create-checkout-session")
+//   .post(isAuthenticated, initiateStripeCheckout);
+// router
+//   .route("/webhook")
+//   .post(express.raw({ type: "application/json" }), handleStripeWebhook);
+// router
+//   .route("/course/:courseId/detail-with-status")
+//   .get(isAuthenticated, getCoursePurchaseStatus);
 
-router.route("/").get(isAuthenticated, getPurchasedCourses);
+// router.route("/").get(isAuthenticated, getPurchasedCourses);
 
-export default router;
+// export default router;
